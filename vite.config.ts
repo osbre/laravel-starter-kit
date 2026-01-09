0b1235f876ec9b16fa97a51e2c2d4fffc2274f56
Ostap Brehin <osbre@protonmail.com>
2026-01-09 05:54:24 +0000

    Replace React with Preact

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [
@@ -9,7 +9,7 @@ export default defineConfig({
       ssr: 'resources/js/ssr.tsx',
       refresh: true,
     }),
-    react(),
+    preact(),
   ],
   resolve: {
     alias: {
